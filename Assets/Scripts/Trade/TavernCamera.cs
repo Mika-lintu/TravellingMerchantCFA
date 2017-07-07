9ae94412e8aa9ad386499808c471fdd214deec36
KAMIT\mikalin <mikalintula@kamk.fi>
2017-07-07 15:02:52 +0300

    Tavern movement & tavern camera

---
+++ Assets/Scripts/Trade/TavernCamera.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TavernCamera : MonoBehaviour {
+
+    public float dampTime;
+    public Transform player;
+    public Transform target;
+    public Vector3 offset;
+    Vector3 velocity = Vector3.zero;
+    Camera cam;
+    bool zoomToPlayer;
+
+    void Awake()
+    {
+        zoomToPlayer = false;
+        cam = Camera.main;
+    }
+	
+	
+	void Update () {
+
+        if (Input.GetKeyDown("a"))
+        {
+            if (!zoomToPlayer)
+            {
+                StopAllCoroutines();
+                StartCoroutine(ZoomToShop(2f));
+            }
+            else
+            {
+                StopAllCoroutines();
+                StartCoroutine(ZoomBack(5f));
+            }
+            
+        }
+		if (target)
+        {
+            Vector3 point = Camera.main.WorldToViewportPoint(target.position);
+            Vector3 delta = target.position - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+            Vector3 destination = transform.position + offset + delta;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+            transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -6f, 5), -10);
+        }
+	}
+
+    IEnumerator ZoomToShop(float zoom)
+    {
+        while (cam.orthographicSize > zoom)
+        {
+            cam.orthographicSize -= Time.deltaTime * 4;
+            dampTime = 0.25f;
+            yield return null;
+        }
+        zoomToPlayer = true;
+        
+
+    }
+    IEnumerator ZoomBack(float zoom)
+    {
+        while (cam.orthographicSize < zoom)
+        {
+            cam.orthographicSize += Time.deltaTime * 4;
+            dampTime = 0.25f;
+            yield return null;
+        }
+
+        zoomToPlayer = false;
+
+    }
+}
---
+++ Assets/Scripts/Trade/TavernMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TavernMovement : MonoBehaviour {
+
+    public GameObject target;
+    public float moveSpeed;
+    bool onMove;
+    bool isFlipped;
+    Vector3 mousePos;
+
+    void Awake()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            OnClick();
+        } else if (onMove == true)
+        {
+            MovePlayer();
+        }
+        if(target.transform.position.x < transform.position.x)
+        {
+            if (!isFlipped)
+            {
+                isFlipped = true;
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, 1);
+            }
+
+        }else
+        {
+            if (isFlipped)
+            {
+                isFlipped = false;
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, 1);
+            }
+        }
+    }
+    void OnClick()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+        
+        if (hit.collider != null)
+        {
+            target.transform.position =  mousePos;
+            onMove = true;
+        }
+       
+        
+    }
+    void MovePlayer()
+    {
+        transform.position = Vector2.Lerp(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+        
+    }
+}
