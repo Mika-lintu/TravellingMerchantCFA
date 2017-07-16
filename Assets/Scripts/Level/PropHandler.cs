1837a53082085fb2160f5f6ece2295b0cb3cb287
Tommi <toronent@gmail.com>
2017-07-16 23:31:17 +0300

    Can't remember... ...I did something though!

--- Assets/Scripts/Level/PropHandler.cs
+++ Assets/Scripts/Level/PropHandler.cs
@@ -76,7 +76,7 @@ public class PropHandler : MonoBehaviour
     {
         for (int i = 0; i < propIDs.Count; i++)
         {
-            poolManager.CreatePool(propDictionary[propIDs[i]], 4);
+            poolManager.CreatePool(propDictionary[propIDs[i]], 8);
         }
     }
 
