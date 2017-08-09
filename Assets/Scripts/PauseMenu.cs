9e3cf7b6dd085e5ddbdb2457b721185a510651a8
KAMIT\mikalin <mikalintula@kamk.fi>
2017-08-09 15:50:44 +0300

    Confict happend

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+    public GameObject portraitMenu;
+    public GameObject landscapeMenu;
+    public GameObject[] portraitMenuPages;
+
+    public GameObject[] landscapeMenuPages;
+
+    void Start () {
+		
+	}
+	
+	public void ClosePagesPortrait()
+    {
+        for (int i = 0; i < portraitMenuPages.Length; i++)
+        {
+            portraitMenuPages[i].SetActive(false);
+        }
+    }
+    public void ClosePagesLandscape()
+    {
+        for (int i = 0; i < landscapeMenuPages.Length; i++)
+        {
+            landscapeMenuPages[i].SetActive(false);
+        }
+    }
+}
